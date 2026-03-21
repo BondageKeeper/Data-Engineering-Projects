92e1a379946c1f1b18bbc5503f747a7a5a67ad58
Sergey <rychalovser@gmail.com>
2026-03-21 16:04:09 +0300

    Create people_data_json_parser.py
    another one

---
+++ json_parsing/people_data_json_parser.py
@@ -0,0 +1,313 @@
+json_data = """[
+    {
+        "id": 25,
+        "firstname": "Emma",
+        "lastname": "Garcia",
+        "email": "emmagarcia@email.com",
+        "birthDate": "1983-09-11",
+        "login": {
+            "uuid": "25aeed09-9430-4d68-901f-c0d4c1c3bf11",
+            "username": "emmagarcia",
+            "password": "jsonplaceholder.org",
+            "md5": "c1328472c5794a25723600f71c1b4586",
+            "sha1": "35544a31cc19bd6520af116554873167117f4d94",
+            "registered": "2021-02-01T08:03:02.390Z"
+        },
+        "address": {
+            "street": "$%123 Oak Street$%$",
+            "suite": "Apt. 130",
+            "city": "Anytown",
+            "zipcode": "12345-6789",
+            "geo": {
+                "lat": "42.1234",
+                "lng": "-71.9012"
+            }
+        },
+        "phone": "+7 912 345-67-89",
+        "website": "ftp://emmagarcia.com",
+        "company": {
+            "name": "GHI Corporation",
+            "catchPhrase": "Innovative solutions for your needs",
+            "bs": "Consulting"
+        }
+    },
+    {
+        "id": 26,
+        "firstname": "Aiden",
+        "lastname": "Martinez",
+        "email": "aidenmartinez@yandex.ru",
+        "birthDate": "1983-09-11",
+        "login": {
+            "uuid": "26aeed09-9430-4d68-901f-c0d4c1c3bf11",
+            "username": "aidenmartinez",
+            "password": "jsonplaceholder.org",
+            "md5": "c1328472c5794a25723600f71c1b4586",
+            "sha1": "35544a31cc19bd6520af116554873167117f4d94",
+            "registered": "2022-02-01T08:03:02.390Z"
+        },
+        "address": {
+            "street": "^&456 Cherry Street^&",
+            "suite": "Apt. 140",
+            "city": "Anytown",
+            "zipcode": "12345-6789",
+            "geo": {
+                "lat": "42.5678",
+                "lng": "-71.2345"
+            }
+        },
+        "phone": "+7 920 345-67",
+        "website": "https://aidenmartinez.com",
+        "company": {
+            "name": "JKL Industries",
+            "catchPhrase": "Innovative solutions for a better world",
+            "bs": "Manufacturing"
+        }
+    },
+    {
+        "id": 27,
+        "firstname": "Chloe",
+        "lastname": "Nguyen",
+        "email": "chloenguyen@email.com",
+        "birthDate": "1988-12-13",
+        "login": {
+            "uuid": "27aeed12-9430-4d68-901f-c0d4c1c3bf13",
+            "username": "chloenguyen",
+            "password": "jsonplaceholder.org",
+            "md5": "c1328472c5794a25723600f71c1b4586",
+            "sha1": "35544a31cc19bd6520af116554873167117f4d94",
+            "registered": "2022-01-03T09:03:02.390Z"
+        },
+        "address": {
+            "street": "  789 Maple Street  ",
+            "suite": "Apt. 150",
+            "city": "Anytown",
+            "zipcode": "12345-6789",
+            "geo": {
+                "lat": "42.9012",
+                "lng": "-71.5678"
+            }
+        },
+        "phone": "+7 912 546-67-89",
+        "website": "http://chloenguyen.com",
+        "company": {
+            "name": "LMN Enterprises",
+            "catchPhrase": "Innovative solutions for your success",
+            "bs": "Technology"
+        }
+    },
+    {
+        "id": 28,
+        "firstname": "Liam",
+        "lastname": "Brown",
+        "email": "liambrown@yandex.ru",
+        "birthDate": "2002-01-25",
+        "login": {
+            "uuid": "28aeed01-9430-4d68-901f-c0d4c1c3bf25",
+            "username": "stefankupidura",
+            "password": "jsonplaceholder.org",
+            "md5": "c1328472c5794a25723600f71c1b4586",
+            "sha1": "35544a31cc19bd6520af116554873167117f4d94",
+            "registered": "2022-01-02T09:00:01.390Z"
+        },
+        "address": {
+            "street": "$$123 Pine Street$$",
+            "suite": "Apt. 160",
+            "city": "Anytown",
+            "zipcode": "12345-6789",
+            "geo": {
+                "lat": "42.1234",
+                "lng": "-71.5678"
+            }
+        },
+        "phone": "345?- 555-1234",
+        "website": "ftps://liambrown.com",
+        "company": {
+            "name": "PQR Corporation",
+            "catchPhrase": "Innovative solutions for your business",
+            "bs": "Consulting"
+        }
+    },
+    {
+        "id": 29,
+        "firstname": "Jasmine",
+        "lastname": "Brown",
+        "email": "jasminebrown@email.com",
+        "birthDate": "1990-11-19",
+        "login": {
+            "uuid": "29aeed11-9430-4d68-901f-c0d4c1c3bf19",
+            "username": "jasminebrown",
+            "password": "jsonplaceholder.org",
+            "md5": "c1328472c5794a25723600f71c1b4586",
+            "sha1": "35544a31cc19bd6520af116554873167117f4d94",
+            "registered": "2021-11-02T07:00:59.390Z"
+        },
+        "address": {
+            "street": "##124 Elm Street#",
+            "suite": "Apt. 110",
+            "city": "Anytown",
+            "zipcode": "12345-6789",
+            "geo": {
+                "lat": "42.3234",
+                "lng": "-73.5670"
+            }
+        },
+        "phone": "+8 956 345-67-89",
+        "website": "https://jasminebrown.com",
+        "company": {
+            "name": "A&B Technology",
+            "catchPhrase": "Innovative IT solutions for your business",
+            "bs": "Technology"
+        }
+    },
+    {
+        "id": 30,
+        "firstname": "Stefan",
+        "lastname": "Kupidura",
+        "email": "#$%stefankupidura@email.com",
+        "birthDate": "1978-12-25",
+        "login": {
+            "uuid": "30aeed12-9430-4d68-901f-c0d4c1c3bf25",
+            "username": "stefankupidura",
+            "password": "jsonplaceholder.org",
+            "md5": "c1328472c5794a25723600f71c1b4586",
+            "sha1": "35544a31cc19bd6520af116554873167117f4d94",
+            "registered": "2021-12-01T07:51:59.390Z"
+        },
+        "address": {
+            "street": "!704 Mary Street!",
+            "suite": "Apt. 160",
+            "city": "Anytown",
+            "zipcode": "12345-6789",
+            "geo": {
+                "lat": "44.5688",
+                "lng": "-72.2645"
+            }
+        },
+        "phone": "+1##76 555-1234",
+        "website": "https://stefankupidura.com",
+        "company": {
+            "name": "ABC Corporation",
+            "catchPhrase": "IT and Software solutions",
+            "bs": "Development"
+        }
+    }
+]"""
+import json , re
+data = json.loads(json_data)
+emails_list = []
+websites_list = []
+address_list = []
+registration_list = []
+uuid_data = []
+user_phone_numbers = []
+for user in data:
+
+    user_email = user['email']
+    emails_list.append(user_email)
+
+    user_website = user['website']
+    websites_list.append(user_website)
+
+    user_address = user['address']
+    del user['address']['suite']
+    del user['address']['zipcode']
+    del user['address']['city']
+    address_list.append(user_address)
+
+    registration_data = user['login']['registered']
+    registration_list.append(registration_data)
+
+    uuid_value = user['login']['uuid']
+    uuid_data.append(uuid_value)
+
+    user_phone = user['phone']
+    user_phone_numbers.append(user_phone)
+
+def sort_secure_protocols(websites):
+    filtered_websites = []
+    for website in websites:
+        if re.findall(r'\bhttps|ftps://[\w.-]+\b',website):
+            filtered_websites.append(f'{website} - secure')
+        else:
+            filtered_websites.append(f'{website} - insecure')
+    return filtered_websites
+security_check = sort_secure_protocols(websites_list)
+
+def sort_user_numbers(given_phones):
+    filtered_phones = []
+    for phone in given_phones:
+        if re.findall(r'(\b7|8)\s*\d{3}\s*\d{3}-\d{2}-\d{2}',phone):
+            filtered_phones.append(f'{phone} - number is active')
+        else:
+            filtered_phones.append(f'{phone} - number is not active')
+    return filtered_phones
+sorted_phone_numbers = sort_user_numbers(user_phone_numbers)
+
+def sort_uuid_date(uuid_transmitter):
+    filtered_uuid = []
+    for uuid in uuid_transmitter:
+        uuid = ''.join(re.findall(r'\d+',uuid))
+        filtered_uuid.append(uuid)
+    return filtered_uuid
+numbers_uuid = sort_uuid_date(uuid_data)
+
+def sort_registration_data(reg_data):
+    filtered_registration = []
+    for reg in reg_data:
+        reg = reg.split('T')[0]
+        filtered_registration.append(reg)
+    return filtered_registration
+result_of_registration = sort_registration_data(registration_list)
+
+def postal_address(address):
+    streets = []
+    geolocation = []
+    count = 0
+    for component in address:
+        for geo_data in component.values():
+            if count == 0:
+                geo_data = geo_data.strip(' %$#^&*@!?').upper()
+                streets.append(geo_data)
+                count += 1
+            elif count == 1:
+                geolocation.append(geo_data)
+                count = 0
+    pairs = list(zip(streets,geolocation))
+    return pairs
+changed_postal_address = postal_address(address_list)
+
+def sort_gmail_emails(emails):
+    filtered_lists = []
+    for email in emails:
+        if re.findall(r'\b[\w.-]+@email\.com\b',email):
+            filtered_lists.append(f'{email} - email is active')
+        else:
+            filtered_lists.append(f'{email} email is not active')
+    return filtered_lists
+sorted_emails = sort_gmail_emails(emails_list)
+
+def final_upload(changed_geo,website_verification,emails,reg):
+    for index , geo_location in enumerate(changed_geo):
+        data[index]['address']['geo_location'] = geo_location
+        del data[index]['address']['street']
+        del data[index]['address']['geo']  #delete obsolete rubbish :)
+    for index , check_websites in enumerate(website_verification):
+        data[index]['website_verification'] = check_websites
+        del data[index]['website']
+    for index , email in enumerate(emails):
+        data[index]['relevant_emails'] = email
+        del data[index]['email']
+    for index , registration in enumerate(reg):
+        data[index]['login']['registration_data'] = registration
+        del data[index]['login']['registered']
+    for index , numbers in enumerate(numbers_uuid):
+        data[index]['login']['numbers_uuid'] = numbers
+        del data[index]['login']['uuid']
+    for index , phone_number in enumerate(sorted_phone_numbers):
+        data[index]['status_phone_number'] = phone_number
+        del data[index]['phone']
+    return data
+established_address = final_upload(changed_postal_address,security_check,sorted_emails,result_of_registration)
+
+with open('postal_credentials.json','w',encoding = 'utf-8') as file:
+    json.dump(data,file,indent=3)
