385e40edef325e5ffb5b62e611cf8a5121c225f1
Sergey <rychalovser@gmail.com>
2026-05-10 01:09:42 +0300

    Create smart_image_loader.py
    That will definitely come in handy . Method THRESH_OTSU carries out all difficult work here . It picks the most suitable threshold of colors so we don't even need to choose these thresholds and waste a bunch of time.

---
+++ OpenCV/smart_image_loader.py
@@ -0,0 +1,14 @@
+import numpy as np
+def smart_otsu_loader(path,nx,ny):
+    image2 = cv2.imread(path,0)
+    if image2 is None:
+        exit()
+    else:
+        image2 = cv2.resize(image2,(nx,ny))
+        _ , binary_value = cv2.threshold(image2,0,1,cv2.THRESH_BINARY + cv2.THRESH_OTSU) #0 - not used but we write it!
+    if np.sum(binary_value) > (nx * ny) / 2:
+        binary_value = 1 - binary_value
+        print('emited!')
+    binary_value = cv2.flip(binary_value,0)
+    return binary_value.astype(np.bool)
+smart_otsu_loader('Clark_wing.png',300,300)
